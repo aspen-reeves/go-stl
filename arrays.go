9225434e1e65f6d8ecba7421198cb88b9bf31ed9
aspen-reeves <aspenbreeves@gmail.com>
2022-10-06 00:21:45 -0500

    ok

--- arrays.go
+++ arrays.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-type group struct {
+type field struct {
 	head *node
 	tail *node
 }
@@ -23,13 +23,16 @@ type dataCell struct {
 	C rune
 }
 
-func initGroup(data ...dataCell) group {
+func initGroup(data ...dataCell) field {
 	//create a new group
-	var newGroup group
+	var newGroup field
 	//create a new node
-	newNode := new(node)
-	newNode.cell = data[0]
-	newNode.next = nil
+	/*
+		newNode := node{}
+		newNode.cell = data[0]
+		newNode.next = nil
+		newNode.index = 0*/
+	newNode := &node{cell: data[0], next: nil, index: 0}
 	//set the head and tail
 	newGroup.head = newNode
 	newGroup.tail = newNode
@@ -47,40 +50,40 @@ func initGroup(data ...dataCell) group {
 	}
 	return newGroup
 }
-func addNode(data dataCell, group group) {
+func (g *field) add(data dataCell) {
 	//create a new node
 	var newNode node
 	newNode.cell = data
 	newNode.next = nil
 	//add the new node to the tail
-	group.tail.next = &newNode
+	g.tail.next = &newNode
 	//update the tail
-	group.tail = &newNode
+	g.tail = &newNode
 }
-func delNode(index int, group group) {
+func (g *field) delete(index int) {
 	//find the node
 	var currentNode node
-	currentNode = *group.head
+	currentNode = *g.head
 	for currentNode.index != index {
 		currentNode = *currentNode.next
 	}
 	//delete the node
 	currentNode = *currentNode.next
 }
-func printGroup(group group) {
+func printGroup(g field) {
 	//print the group
 	var currentNode node
-	currentNode = *group.head
+	currentNode = *g.head
 	for currentNode.next != nil {
 		fmt.Println(currentNode.cell)
 		currentNode = *currentNode.next
 	}
 	fmt.Println(currentNode.cell)
 }
-func readNode(index int, group group) dataCell {
+func (g field) at(index int) dataCell {
 	//find the node
 	var currentNode node
-	currentNode = *group.head
+	currentNode = *g.head
 	for currentNode.index != index {
 		if currentNode.next == nil {
 			return dataCell{}
@@ -90,68 +93,3 @@ func readNode(index int, group group) dataCell {
 	//return the cell
 	return currentNode.cell
 }
-
-/*func groupSort(b []group) []group {
-	//sort the array using bogo sort
-	//convert the array to an array of ints
-
-	intArray := make([]int, len(b)*5)
-	j := 0
-	for i := 0; i < len(intArray); i += 5 {
-		a := groupAt(j, b)
-		intArray[i] = a.I
-		if a.B {
-			intArray[i+1] = 1
-		} else {
-			intArray[i+1] = 0
-		}
-		temp, err := strconv.Atoi(a.S)
-		if err != nil {
-			intArray[i+2] = 0
-		} else {
-			intArray[i+2] = temp
-		}
-		intArray[i+3] = int(a.F)
-		intArray[i+4] = int(a.C)
-		j++
-	}
-	fmt.Println(intArray)
-	intArray = sort(intArray)
-	fmt.Println(intArray)
-	a := make([]group, len(intArray)/5)
-	//convert the array of ints back to an array of groups
-	j = 0
-	for i := 0; i < len(intArray); i += 5 {
-		a[j].index = j
-		a[j].value.I = intArray[i]
-		if intArray[i+1] == 1 {
-			a[j].value.B = true
-		} else {
-			a[j].value.B = false
-		}
-		a[j].value.S = strconv.Itoa(intArray[i+2])
-		a[j].value.F = float64(intArray[i+3])
-		a[j].value.C = rune(intArray[i+4])
-		j++
-	}
-
-	return a
-}
-
-func sort(a []int) []int {
-	//sort the array using bogo sort
-	rand.Seed(time.Now().UnixNano())
-	for !isSorted(a) {
-		rand.Shuffle(len(a), func(i, j int) { a[i], a[j] = a[j], a[i] })
-	}
-	return a
-}
-func isSorted(a []int) bool {
-	for i := 0; i < len(a)-1; i++ {
-		if a[i] > a[i+1] {
-			return false
-		}
-	}
-	return true
-}
-*/
--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
 	//create a new group
-	var newGroup group
+	var newGroup field
 	//insert data
 	newGroup = initGroup(dataCell{0, false, "no", 1.1, 'a'}, dataCell{1, true, "hello", 1.1, 'a'}, dataCell{2, false, "world", 2.2, 'b'}, dataCell{3, true, "hello", 3.3, 'c'}, dataCell{4, false, "world", 4.4, 'd'}, dataCell{5, true, "hello", 5.5, 'e'}, dataCell{6, false, "world", 6.6, 'f'}, dataCell{7, true, "hello", 7.7, 'g'}, dataCell{8, false, "world", 8.8, 'h'}, dataCell{9, true, "hello", 9.9, 'i'}, dataCell{10, false, "world", 10.10, 'j'})
 
 	//print the group
-	fmt.Println(readNode(5, newGroup))
 }
